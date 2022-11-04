9345bd9b563f2aa1ba52084cd3a87fc5915b7db5
qhy040404 <qhy040404@163.com>
2022-11-04 16:11:49 +0800

    Initial commit

---
+++ src/1.1.R
@@ -0,0 +1,30 @@
+# 以数据框的形式创建上述数据，定义数据名字为leadership，并将其分别存为R格式和csv格式
+
+person <- c(1, 2, 3, 4, 5)
+date <- c("10/24/14", "10/28/14", "10/01/14", "10/12/14", "05/01/14")
+region <- c("US", "US", "UK", "UK", "UK")
+gender <- c("M", "F", "F", "M", "F")
+old <- c(32, 45, 25, 39, 99)
+q1 <- c(5, 3, 3, 3, 2)
+q2 <- c(4, 5, 5, 3, 2)
+q3 <- c(5, 2, 5, 4, 1)
+q4 <- c(5, 5, 5, NA, 2)
+q5 <- c(5, 5, 2, NA, 1)
+leadership <-
+  data.frame(
+    经理人 = person,
+    日期 = date,
+    国籍 = region,
+    性别 = gender,
+    年龄 = old,
+    q1 = q1,
+    q2 = q2,
+    q3 = q3,
+    q4 = q4,
+    q5 = q5
+  )
+
+leadership
+
+save(leadership, file = "leadership.RData")
+write.csv(leadership,"leadership.csv")
---
+++ src/1.3.R
@@ -0,0 +1,3 @@
+# 对leadership数据判断是否有缺失元素
+
+is.na(leadership)
